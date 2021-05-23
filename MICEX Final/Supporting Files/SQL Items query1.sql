a7f9d7467f456945fbb4ea8d4f2ee188f585408f
beatrizqa <beatriz.manzano@qa.com>
2021-05-23 19:12:10 +0100

    BM:Add MICEX Final to Git

---
+++ MICEX Final/Supporting Files/SQL Items query1.sql
@@ -0,0 +1,3 @@
+INSERT INTO items
+       VALUES (1,'Eiffel Tower',14,5,3586,'2021-03-05','NULL','NULL');
+		
---
+++ MICEX/Supporting Files/SQL Items queryworking.sql
@@ -0,0 +1,2 @@
+INSERT INTO exhibitions (name,description,exhDate,exhDuration,exhLocation)
+VALUES ('Miscellaneous','Didn\'t know what to do with them','2021-10-29',5,'Madrid');
