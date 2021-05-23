284e2b91aa39f3230e16d6b0a353783446b536a6
beatrizqa <beatriz.manzano@qa.com>
2021-05-23 06:59:00 +0100

    BM: push all files for new VM

--- MICEX/application/__init__.py
+++ MICEX/application/__init__.py
@@ -7,8 +7,8 @@ from application import templates
 
 app = Flask(__name__)
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@35.234.144.82/miniblocksexhibitions"
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@34.89.84.151:5000/dbmicex.db"
+#app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
 app.config["SECRET_KEY"] = "jksdhk"
 
 db = SQLAlchemy(app)
